2dad6ac7ea8c1402f912542001f90892067c2a9b
Marmik Patel <marmikpatel250@gmail.com>
2022-07-09 22:04:44 +0530

    added model to admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ActionCategory, ActionDetail
+from .models import ActionCategory, ActionDetail, ActionVoice
 
 admin.site.register(ActionCategory)
 admin.site.register(ActionDetail)
+admin.site.register(ActionVoice)
